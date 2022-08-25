26ff4546ae9b5172e44bb9606b950f91071688cf
Rodrigo Fontes <rodrigo_fnts@hotmail.com>
2022-08-24 23:12:58 -0300

    Dia 9: desafio completado com sucesso

---
+++ day-nine/scripts.js
@@ -0,0 +1,22 @@
+const loader = () => {
+  const progressBar = document.getElementsByClassName("progress")[0];
+
+  let width = 1;
+  let count = setInterval(doProgress, 300);
+
+  function doProgress() {
+    if (width >= 100) {
+      clearInterval(count);
+    } else {
+      width++;
+
+      progressBar.style.width = `${width}%`;
+
+      if (width === 100) {
+        progressBar.classList.add("loaded");
+      }
+    }
+  }
+};
+
+loader();
